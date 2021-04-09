65ba68f4eb557987c3e2b4e91dbf5fb94ca0240b
sayoi341 <20alt341@gmail.com>
2021-04-09 21:35:47 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# install with `npm install`
+
+上のコマンドで`node_modules`をインストールしてください
+
+## `npm start`
+
+開発用のローカルサーバーの起動
+
+## `npm run build`
+
+`dist`下にビルドされます
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React, {FC} from "react";
+import {render} from "react-dom";
+
+
+const Index:FC = () => {
+    return(<></>)
+}
+
+render(<Index />, document.getElementById("root"));
